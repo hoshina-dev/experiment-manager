1e829b933db6fd3b6e7d7c15a8368b41fb075f2a
Chantouch Narasetaphan <inecrft4747@gmail.com>
2026-05-17 21:05:17 +0700

    Move script to .up.sql and add .down.sql

---
+++ migrations/001_initial_schema.down.sql
@@ -0,0 +1,11 @@
+DROP TRIGGER IF EXISTS trg_experiments_updated_at ON experiments;
+DROP TRIGGER IF EXISTS trg_experiment_templates_updated_at ON experiment_templates;
+DROP TRIGGER IF EXISTS trg_sample_types_updated_at ON sample_types;
+
+DROP FUNCTION IF EXISTS set_updated_at();
+
+DROP TABLE IF EXISTS experiments;
+DROP TABLE IF EXISTS experiment_templates;
+DROP TABLE IF EXISTS sample_types;
+
+DROP EXTENSION IF EXISTS "uuid-ossp";
