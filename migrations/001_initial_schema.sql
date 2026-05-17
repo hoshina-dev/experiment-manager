128936821304613713242ef2be9f0548ef3d1672
Chantouch Narasetaphan <inecrft4747@gmail.com>
2026-05-17 20:50:08 +0700

    Collapse user form, worker form and calculations columns into one big json column. And update table names

--- migrations/001_initial_schema.sql
+++ migrations/001_initial_schema.sql
@@ -1,3 +1,7 @@
+-- This design has 1 assumption: Ticketing service database will have at least 2 tables:
+-- 1. tickets               contain status of each ticket.
+-- 2. ticket_experiments    many2many relation of each ticket with experiment templates. For example, ticket X has to do Proximate analysis & Calorific analysis
+
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- High-level category: Coal, Soil, Water, etc.
@@ -10,52 +14,33 @@ CREATE TABLE sample_types (
     deleted_at      TIMESTAMPTZ
 );
 
--- Specific analysis within a type
+-- Specific experiment within a type
 -- e.g. Coal -> "Proximate analysis", "Calorific value"
-CREATE TABLE analyses (
-    id                  UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    sample_type_id      UUID NOT NULL REFERENCES sample_types(id) ON DELETE RESTRICT,
-    name                TEXT NOT NULL,
-    description         TEXT,
+CREATE TABLE experiment_templates (
+    id              UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    sample_type_id  UUID NOT NULL REFERENCES sample_types(id) ON DELETE RESTRICT,
+    name            TEXT NOT NULL,
+    description     TEXT,
 
-    user_inputs         JSONB,
+    template        JSONB NOT NULL, -- จท
 
-    -- e.g. [{"code": "crucible_mass", "label": "Crucible Mass (g)", ...}, {...}, ...]
-    analysis_inputs     JSONB NOT NULL DEFAULT '[]'::jsonb,
-
-    -- e.g. [{"code": "moisture_loss", "label": "Moisture Loss (%)", "formula": "(sample_mass - mass_after_moisture) / (sample_mass * 100)"}, {...}, ...]
-    calculations        JSONB NOT NULL DEFAULT '[]'::jsonb,
-
-    created_at          TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    updated_at          TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    deleted_at          TIMESTAMPTZ,
+    created_at      TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at      TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    deleted_at      TIMESTAMPTZ,
     UNIQUE (sample_type_id, name)
 );
 
-CREATE INDEX idx_analyses_sample_type ON analyses(sample_type_id);
+CREATE INDEX idx_experiment_templates_sample_type ON experiment_templates(sample_type_id);
 
 -- State of input form for each ticket
-CREATE TABLE job_inputs (
-    id                      UUID PRIMARY KEY, -- Uses the same id as ticket_analyses id, as it has 1-1 relation.
+CREATE TABLE experiments (
+    id           UUID PRIMARY KEY, -- Uses the same id as ticket_analyses id, as it has 1-1 relation.
 
-    user_form_state         JSONB NOT NULL,
-    technician_form_state   JSONB NOT NULL,
+    state        JSONB NOT NULL,
 
-    created_at              TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    updated_at              TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    deleted_at              TIMESTAMPTZ,
-);
-
-CREATE INDEX idx_job_inputs_job_analysis ON job_inputs(job_id);
-
--- Final reportable values.
--- Produced by evaluating calculation formulas against job_inputs.
-CREATE TABLE job_results (
-    id                  UUID PRIMARY KEY,   -- Uses the same id as ticket_analyses id, as it has 1-1 relation.
-    value               JSONB NOT NULL,     -- e.g. [{"code": "moisture_loss", "label": "Moisture Loss (%)", "result": 32}, {...}, ...]
-    created_at          TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    updated_at          TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    deleted_at          TIMESTAMPTZ,
+    created_at   TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at   TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    deleted_at   TIMESTAMPTZ,
 );
 
 -- updated_at triggers
